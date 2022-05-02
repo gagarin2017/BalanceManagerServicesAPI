9796b8d7aefbba4d683d55d67efa28bd04a43ced
Yury Gagarin <yury_gagarin2017@mail.ru>
2022-05-02 15:10:50 +0100

    - fixing duplicate records bug

--- src/main/java/com/greenland/balancemanager/repositories/TxRowRepository.java
+++ src/main/java/com/greenland/balancemanager/repositories/TxRowRepository.java
@@ -1,13 +1,29 @@
 package com.greenland.balancemanager.repositories;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.greenland.balancemanager.domain.TxCategory;
 import com.greenland.balancemanager.domain.TxRow;
+import com.greenland.balancemanager.domain.TxTag;
 
 @Repository
 public interface TxRowRepository extends CrudRepository<TxRow, Long	>{
 
 	Iterable<TxRow> findByTxBankAccountTxBankAccountId(long bankAccountId);
 
+	Optional<TxRow> findByTxBankAccountTxBankAccountIdAndTxDateAndTxDescriptionTxDescriptionIdAndTxAmount(final Long txBankAccountId, final LocalDate txDate,
+			final Long txDescriptionId, final BigDecimal txAmount);
+
+	@Modifying
+	@Query("update TxRow u set u.txMemo = :memo, u.txCategory = :category, u.txTag = :tag where u.txRowId = :id")
+	void updateTransaction(@Param(value = "id") long id, @Param(value = "memo") String memo,
+			@Param(value = "category") TxCategory category, @Param(value = "tag") TxTag tag);
 }
--- src/main/java/com/greenland/balancemanager/services/TxRowService.java
+++ src/main/java/com/greenland/balancemanager/services/TxRowService.java
@@ -1,11 +1,14 @@
 package com.greenland.balancemanager.services;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.greenland.balancemanager.domain.TxBankAccount;
 import com.greenland.balancemanager.domain.TxCategory;
@@ -16,6 +19,7 @@ import com.greenland.balancemanager.domain.TxTag;
 import com.greenland.balancemanager.repositories.TxRowRepository;
 
 @Service
+@Transactional
 public class TxRowService {
 	
 	@Autowired
@@ -42,12 +46,14 @@ public class TxRowService {
 
 	public Iterable<TxRow> save(final List<TxRow> inputTxData) {
 		
+		final Set<TxRow> savedTransactions = new HashSet<>();
+		
 		inputTxData.forEach(inputTx -> {
 			
 			final String inputDesc = inputTx.getTxDescription()!= null ? inputTx.getTxDescription().getDescription() : null;
 			final String inputCategoryName = inputTx.getTxCategory() != null ? inputTx.getTxCategory().getCategoryName() : null;
 			final String inputTagName = inputTx.getTxTag() != null ? inputTx.getTxTag().getTagName() : null;
-			
+
 			final Optional<TxBankAccount> savedAccount = bankAccountService
 					.getBankAccountById(inputTx.getTxBankAccount().getTxBankAccountId());
 			
@@ -76,9 +82,22 @@ public class TxRowService {
 				inputTx.setTxTag(savedTag.get());
 			}
 			
+			// check if transaction already exists
+			// TODO: consider two very same transactions which happened on the very same day. Then you will want a new tx added, not updated
+			final Optional<TxRow> existingTransactionRow = txRowRepository.findByTxBankAccountTxBankAccountIdAndTxDateAndTxDescriptionTxDescriptionIdAndTxAmount(inputTx.getTxBankAccount().getTxBankAccountId(),
+					inputTx.getTxDate(), inputTx.getTxDescription().getTxDescriptionId(), inputTx.getTxAmount());
+			
+			if (existingTransactionRow.isPresent()) {
+				// then it will update
+				txRowRepository.updateTransaction(existingTransactionRow.get().getTxRowId(), inputTx.getTxMemo(), inputTx.getTxCategory(), inputTx.getTxTag());
+			} else {
+				savedTransactions.add(txRowRepository.save(inputTx));
+			}
+			
+			
 		});
 		
-		return txRowRepository.saveAll(inputTxData);
+		return savedTransactions;
 	}
 
 }
